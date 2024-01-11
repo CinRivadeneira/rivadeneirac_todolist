40a34b2df6547c35045cddaddf82a623916a73c3
cistri <cistri092@gmail.com>
2024-01-11 17:36:19 -0300

    actualizado

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,10 +6,12 @@ function TaskList({ tasks, onDeleteTask, onChangeStatus }) {
     return (
         <table>
             <thead>
+                <tr>
                 <th>ID</th>
                 <th>TAREA</th>
                 <th>COMPLETAR</th>
                 <th>ELIMINAR</th>
+                </tr>
             </thead>
             <tbody>
                 {tasks.map(t => <Task key={t.id} task={t} onDeleteTask ={onDeleteTask} onChangeStatus= {onChangeStatus} />)}
